9bb2c3c1ae5dbc2dadab20c895b6da1d2054cec2
Sudhanva S <40983389+RobotBramhana@users.noreply.github.com>
2026-02-08 17:53:04 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+herName: "Harshitha",
+title: "Hey",
+message: "You make my days brighter and my life happier. Would you like to be my Valentine?",
+yesResponse: "Best decision ever ❤️"
+};
